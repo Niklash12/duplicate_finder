adf81045e17691e657dadbf572e32af76cfde54e
Oliver Ring <oliver@tag010061024113.uni-regensburg.de>
2025-01-08 16:07:25 +0100

    Oliver

--- find_duplicates.py
+++ find_duplicates.py
@@ -31,7 +31,10 @@ def get_file_size(file_path: str) -> int:
         int or None: The file size in bytes, or None if the file cannot be accessed.
     """
     # Run "pytest find_duplicates.py -k get_file_size" to test your implementation
-    raise NotImplementedError()
+    try:
+        return os.path.getsize(file_path)
+    except (OSError, FileNotFoundError):
+        return -1
 
 
 def hash_first_1k_bytes(file_path: str) -> str:
